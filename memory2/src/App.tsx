eae8e54d2a7548b6400f7848fe66aae0d929716e
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-07-21 20:08:22 +0200

    dodano routing

--- memory2/src/App.tsx
+++ memory2/src/App.tsx
@@ -2,14 +2,25 @@ import React from 'react';
 import LandingPage from "./components/LandingPage";
 import GamePage from "./components/GamePage";
 import './App.css';
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch,
+} from "react-router-dom";
 
 const App: React.FC = () => {
     return (
-        <div className="App">
-            <h1>App.tsx</h1>
-            <LandingPage />
-            <GamePage />
-        </div>
+        <Router>
+            <Switch>
+                <Route exact path="/">
+                    <LandingPage />
+                </Route>
+                <Route exact path="/gamePage">
+                    <GamePage />
+                </Route>
+                <Route path="*">{() => "404 NOT FOUND"}</Route>
+            </Switch>
+        </Router>
     );
 }
 
