11e5c5f057ee9471047924f27dfbd52f91d5268d
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2021-07-22 20:50:09 +0200

    zamiana BrowserRouter na HashRouter przed deploymentem

--- memory2/src/App.tsx
+++ memory2/src/App.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+    // BrowserRouter as Router,
+    HashRouter as Router,
+    Route,
+    Switch
+} from "react-router-dom";
 
 import GamePage from "./components/GamePage";
 import LandingPage from "./components/LandingPage";
